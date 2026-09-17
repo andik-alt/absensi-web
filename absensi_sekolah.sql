5a6604b598a0cda8cc37ca9782289f27b0976132
Andik D.s <andikpmk112@gmail.com>
2026-09-17 08:43:48 +0700

    Add files via upload

---
+++ absensi_sekolah.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 17, 2026 at 03:40 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `absensi_sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id` int(11) NOT NULL,
+  `nisn` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` time NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`id`, `nisn`, `tanggal`, `jam`, `status`) VALUES
+(1, '122222222', '2026-08-20', '10:15:48', 'Hadir'),
+(2, '133333', '2026-08-20', '08:33:44', 'Hadir'),
+(3, '1555555', '2026-08-21', '08:43:07', 'Hadir'),
+(4, '1666666', '2026-08-21', '08:44:14', 'Hadir'),
+(5, '8991002122000', '2026-08-22', '09:45:53', 'Hadir'),
+(6, '8994464800004', '2026-09-14', '10:47:58', 'Hadir'),
+(7, '8992745000112', '2026-09-14', '10:49:10', 'Hadir'),
+(8, '1372138273', '2026-09-15', '07:22:59', 'Hadir'),
+(9, '1372138273', '2026-09-16', '10:09:30', 'Hadir'),
+(10, '2748727482', '2026-09-16', '11:22:26', 'Hadir'),
+(11, '8992745000112', '2026-09-17', '08:23:48', 'Terlambat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(600) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(7, 'admin', '$2y$10$.qCQMGOrsxSJec59A.yFN.Jlfsw0..JNiDHza.nrOq58VnA2AJwT2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL,
+  `nisn` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `kelas` varchar(20) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id`, `nisn`, `nama`, `kelas`, `jurusan`, `foto`) VALUES
+(12, '2748727482', 'malaww', 'XII', 'rpl2', NULL),
+(13, '1372138273', 'ANDIK', 'XII', 'rpl2', NULL),
+(14, '8992745000112', 'yuda', 'XII', 'rpl2', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nisn` (`nisn`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
